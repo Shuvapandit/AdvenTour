ac767c7d21febba4b3e8f329ab8f2bb6936e5663
shuva <chakrabortyshuva216@gmail.com>
2022-04-17 15:47:53 +0600

    services price added

--- src/Pages/Home/Home/Service/Service.js
+++ src/Pages/Home/Home/Service/Service.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './Service.css'
 
 const Service = ({service}) => {
-    const{name,img,description}=service;
+    const{name,img,description,price}=service;
     return (
         <div className="service">
             <img className='w-100' src={img} alt=""/>
             <h2>{name}</h2>
             <p>{description}</p>
+            <p>Price:{price}</p>
 
              <button className='btn btn-primary'>Book: {name}</button>
         </div>
