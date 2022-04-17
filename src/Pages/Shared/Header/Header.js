d4e43d4d5dc3b803225efd4db1e67cf4954ef6c8
shuva <chakrabortyshuva216@gmail.com>
2022-04-17 20:26:23 +0600

    header section added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
         <header>
-            <h2>This is header</h2>
+            <nav className='header'>
+            <p className='text-white font-weight-bold h2'>Adven-Tour..</p>
+            {/* 4.import link from routing for no loading  */}
+            <div>
+                <Link to="/home">Home</Link>
+                
+                
+                
+            
+                
+            </div>
+        </nav>
         </header>
             
         
