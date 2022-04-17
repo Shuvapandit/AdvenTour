cdf708ed9bd3a37514a14e1f1345ee6f0837a688
shuva <chakrabortyshuva216@gmail.com>
2022-04-18 02:03:38 +0600

    login and signup page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
@@ -16,6 +17,7 @@ function App() {
       <Route path="/" element={<Home></Home>}></Route>
       <Route path="/home" element={<Home></Home>}></Route>
       <Route path="/login" element={<Login></Login>}></Route>
+      <Route path="/register" element={<Register></Register>}></Route>
       
       <Route path="*" element={<NotFound></NotFound>}></Route>
 
