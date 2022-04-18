d13dc7c1f7d7549ea23df66d22347349beaded3b
shuva <chakrabortyshuva216@gmail.com>
2022-04-18 15:33:05 +0600

    login and reg page error handle and password reset added

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,8 +1,9 @@
 import React, { useRef } from 'react';
 import { Form, Button } from 'react-bootstrap';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 
 const Login = () => {
@@ -12,7 +13,7 @@ const Login = () => {
     const location = useLocation();
 
     let from = location.state?.from?.pathname || "/";
-
+    let errorElement;
 
     const [
       signInWithEmailAndPassword,
@@ -20,9 +21,15 @@ const Login = () => {
       loading,
       error,
     ] = useSignInWithEmailAndPassword(auth);
+    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+
     if (user) {
       navigate(from, { replace: true });
   }
+  if (error) {
+    errorElement = <p className='text-danger'>Error: {error?.message}</p>
+}
+
     const handleSubmit = event => {
         event.preventDefault();
         const email = emailRef.current.value;
@@ -34,6 +41,17 @@ const Login = () => {
     const navigateRegister = event => {
         navigate('/register');
     }
+    const resetPassword = async () => {
+      const email = emailRef.current.value;
+      if (email) {
+          await sendPasswordResetEmail(email);
+         
+      }
+      else{
+         
+      }
+  }
+
 
     return (
         <div className='container w-50 mx-auto mt-5'>
@@ -42,25 +60,22 @@ const Login = () => {
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
     <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
+   
   </Form.Group>
 
   <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
     <Form.Control  ref={passwordRef} type="password" placeholder="Password" required/>
   </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
+  
+  <Button variant="primary w-50 mx-auto d-block mb-2" type="submit">
+    LogIn
   </Button>
 </Form>
+{errorElement}
 <p>New to Adven-Tour..?<Link to="/register" className='text-danger pe-auto text-decoration-none' onClick={navigateRegister}>Please Register</Link></p>
-           
-            
+<p>Forget Password? <button className='btn btn-link text-primary pe-auto text-decoration-none' onClick={resetPassword}>Reset Password</button> </p>        
+         <SocialLogin></SocialLogin>   
         </div>
     );
 };
